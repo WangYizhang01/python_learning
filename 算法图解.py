3f7ad0c55f118ed5aab3219ef9c88f0c9e1015c8
another_one <1739601638@qq.com>
2019-07-15 10:03:28 +0800

    新增makeyourownnerualnetwork文件夹

--- 算法图解.py
+++ 算法图解.py
@@ -70,14 +70,121 @@
 # l = [1,2,3,4,5,6,7,8,9]
 # print(sum(l))
 
-# 递归实现快速排序
-def quicksort(array):
-    if len(array) < 2:
-        return array
-    else:
-        pivot = array[0]
-        less = [i for i in array[1:] if i <= pivot]
-        greater = [i for i in array[1:] if i > pivot]
-        return quicksort(less) + [pivot] + quicksort(greater)
+# # 递归实现快速排序
+# def quicksort(array):
+#     if len(array) < 2:
+#         return array
+#     else:
+#         pivot = array[0]
+#         less = [i for i in array[1:] if i <= pivot]
+#         greater = [i for i in array[1:] if i > pivot]
+#         return quicksort(less) + [pivot] + quicksort(greater)
+#
+# print(quicksort([10,5,2,3]))
 
-print(quicksort([10,5,2,3]))
+# 散列表在Python中可用字典实现
+# from collections import deque
+#
+# graph = {}
+#
+# graph['you'] = ['alice','bob']
+# graph['bob'] = ['anuj','peggy']
+# graph['alice'] = ['peggy']
+# graph['claire'] = ['thom','jonny']
+# graph['anuj'] = []
+# graph['peggy'] = []
+# graph['thom'] = []
+# graph['jonny'] = []
+#
+# graph['you'].append('claire')
+#
+# search_queue = deque()
+# search_queue += graph['you']
+#
+# def person_is_seller(name):
+#     return name[-1] == 'm'
+#
+# def search_mango(search_queue):
+#     while search_queue:
+#         person = search_queue.popleft()
+#         if person_is_seller(person):
+#             print(person + " is a mango seller!")
+#             return True
+#         else:
+#             search_queue += graph[person]
+#     else:
+#         print("There is no mango seller!")
+#         return False
+#
+# print(search_mango(search_queue))
+#
+# # 书中原代码
+# def search(name):
+#     search_queue = deque()
+#     search_queue += graph[name]
+#     searched = []
+#     while search_queue:
+#         person = search_queue.popleft()
+#         if person not in searched:
+#             if person_is_seller(person):
+#                 print person + " is a mango seller!"
+#                 return True
+#             else:
+#                 search_queue += graph[person]
+#                 searched.append(person)
+#     return False
+
+
+# 狄克斯特拉算法实现(未理解)
+graph ={}
+
+graph["start"] = {}
+graph["start"]["a"] = 6
+graph["start"]["b"] = 2
+
+graph["a"] = {}
+graph["a"]["fin"] = 1
+
+graph["b"] = {}
+graph["b"]["a"] = 3
+graph["b"]["fin"] = 5
+
+graph["fin"] = {}
+
+# 创建开销表
+infinity = float("inf")
+costs = {}
+costs["a"] = 6
+costs["b"] = 2
+costs["fin"] = infinity
+
+# 创建一个存储父节点的散列表
+parents = {}
+parents["a"] = "start"
+parents["b"] = "start"
+parents["fin"] = None
+
+processed = []
+
+def find_lowest_cost_node(costs):
+    lowest_cost = float("inf")
+    lowest_cost_node = None
+    for node in costs:
+        cost = costs[node]
+        if cost < lowest_cost and node not in processed:
+            lowest_cost = cost
+            lowest_cost_node = node
+    return lowest_cost_node
+
+node = find_lowest_cost_node(costs)
+while node is not None:
+    cost = costs[node]
+    neighbors = graph[node]
+    for n in neighbors.keys():
+        new_cost = cost + neighbors[n]
+        if costs[n] > new_cost:
+            costs[n] = new_cost
+            parents[n] = node
+    processed.append(node)
+    node = find_lowest_cost_node(costs)
+
